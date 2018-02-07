a22f0e1277ff2cf993e9a7d16b101527b9e5c163
wentoos <yuwt@shinetechchina.com>
2018-02-07 16:50:15 +0800

    ok

--- src/app/new-group-step2/new-group-step2.component.ts
+++ src/app/new-group-step2/new-group-step2.component.ts
@@ -8,7 +8,13 @@ import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 })
 export class NewGroupStep2Component {
   constructor(public dialog: MatDialog) { }
-
+typesOfShoes = [
+    {name:'Boots',face:'https://i.stack.imgur.com/isckt.jpg?s=32&g=1',id:'1'}, 
+    {name:'Clogs',face:'https://i.stack.imgur.com/isckt.jpg?s=32&g=1',id:'2'}, 
+    {name:'Loafers',face:'https://i.stack.imgur.com/isckt.jpg?s=32&g=1',id:'3'}, 
+    {name:'Moccasins',face:'https://i.stack.imgur.com/isckt.jpg?s=32&g=1',id:'4'}, 
+    {name:'Sneakers',face:'https://i.stack.imgur.com/isckt.jpg?s=32&g=1',id:'5'}, 
+  ];
   // openDialog(): void {
   //   let dialogRef = this.dialog.open(DialogOverviewExampleDialog,{
   //     width:'250px',
