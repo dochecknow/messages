9f17a27137695593fa52e0da1fb2e88a5370976b
wentoo <1217167967@qq.com>
2018-04-16 16:46:04 +0800

    messages srcoll

--- src/app/message/message.component.ts
+++ src/app/message/message.component.ts
@@ -11,4 +11,14 @@ export class MessageComponent implements OnInit {
     constructor() { }
     ngOnInit() {
     }
+    list=[
+        {
+            use:'me',
+            text: 'Note: It is possible to use template-driven forms instead, if you prefer. We use reactive forms in this example because it makes subscribing to changes in the inputs value easy.For this example, be sure to import ReactiveFormsModule from @angular/forms into your NgModule. If you are unfamiliar with using reactive forms, you can read more about the subject in the Angular documentation.'
+        },
+        {
+            use: 'she',
+            text: 'Note: It is possible to use template-driven forms instead, if you prefer. We use reactive forms in this example because it makes subscribing to changes in the inputs value easy.For this example, be sure to import ReactiveFormsModule from @angular/forms into your NgModule. If you are unfamiliar with using reactive forms, you can read more about the subject in the Angular documentation.'
+        }
+    ]
 }
--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -25,5 +25,29 @@ export class MessagesComponent implements OnInit {
             time:'12:20',
             id:'adsx1'
         }
+        ,
+        {
+            name: 'Ricky Bobby',
+            ico: 'https://headpic.lrts.me/chensongwusheng?imageMogr/v2/auto-orient/thumbnail/180x180&e=1524636000&token=OOHK9_MIwdSJxAHYi5os2taDVS13CVvcEa1cZDb9:2UpXh1Et6v-S0_j0eU7X-1iitLs=',
+            title: 'headpic .lrts .me/chens ongwus heng?ima geMogr',
+            time: '12:20',
+            id: 'adsx1'
+        }
+        ,
+        {
+            name: 'Ricky Bobby',
+            ico: 'https://headpic.lrts.me/chensongwusheng?imageMogr/v2/auto-orient/thumbnail/180x180&e=1524636000&token=OOHK9_MIwdSJxAHYi5os2taDVS13CVvcEa1cZDb9:2UpXh1Et6v-S0_j0eU7X-1iitLs=',
+            title: 'headpic .lrts .me/chens ongwus heng?ima geMogr',
+            time: '12:20',
+            id: 'adsx1'
+        }
+        ,
+        {
+            name: 'Ricky Bobby',
+            ico: 'https://headpic.lrts.me/chensongwusheng?imageMogr/v2/auto-orient/thumbnail/180x180&e=1524636000&token=OOHK9_MIwdSJxAHYi5os2taDVS13CVvcEa1cZDb9:2UpXh1Et6v-S0_j0eU7X-1iitLs=',
+            title: 'headpic .lrts .me/chens ongwus heng?ima geMogr',
+            time: '12:20',
+            id: 'adsx1'
+        }
     ]
 }
